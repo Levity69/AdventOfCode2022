f89b114e3afd23e7e5a713c94da28d8584b620cc
Tomas <tomukasluk@gmail.com>
2022-12-01 20:12:36 +0200

    Day1

---
+++ Day1/Day1/BiggestNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class BiggestNumbers
+    {
+        public int[] Array = new int[3];
+
+        public BiggestNumbers() { }
+
+        public void addNumber(int number)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                int current = Array[i];
+                if (current < number)
+                {
+                    Array[i] = number;
+                    addNumber(current);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Day1/Day1/Start.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class Start
+    {
+
+        public const string INPUT_PATH = "C:\\Users\\Tomas\\Desktop\\AdventOfCode\\Day1\\Day1\\input.txt";
+
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part2()
+        {
+            BiggestNumbers biggerNumber = new BiggestNumbers();
+            int current = 0;
+
+            using (StreamReader sr = new StreamReader(INPUT_PATH))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (String.IsNullOrEmpty(line))
+                    {
+                        biggerNumber.addNumber(current);
+                        current = 0;
+                    }
+                    else
+                    {
+                        current += int.Parse(line);
+                    }
+                }
+            }
+
+            biggerNumber.addNumber(current);
+
+            Console.WriteLine(biggerNumber.Array.Sum());
+        }
+
+        static void Part1()
+        {
+            int previousBiggest = 0;
+            int current = 0;
+
+            using (StreamReader sr = new StreamReader(INPUT_PATH))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (String.IsNullOrEmpty(line)){
+                        if (current > previousBiggest)
+                        {
+                            previousBiggest = current;
+                        }
+
+                        current = 0;
+                    }
+                    else
+                    {
+                        current += int.Parse(line);
+                    }
+                }
+            }
+
+            if(current > previousBiggest)
+            {
+                previousBiggest= current;
+            }
+
+            Console.WriteLine(previousBiggest);
+        }
+    }
+}
